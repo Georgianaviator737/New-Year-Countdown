aff55c48165a1131bc47d89a1f93209cb1f428e9
Nikoloz Okitashvili <georgianaviator737@gmail.com>
2023-12-28 21:14:58 +0400

    initial upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+function updateCountdown() {
+    const now = new Date();
+    const newYear = new Date('01/01/2024');
+
+    const timeRemaining = newYear - now;
+    const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+    document.getElementById('days').textContent = days;
+    document.getElementById('hours').textContent = hours;
+    document.getElementById('minutes').textContent = minutes;
+    document.getElementById('seconds').textContent = seconds;
+}
+
+function startCountdown() {
+    updateCountdown();
+    setInterval(updateCountdown, 1000);
+}
+
+
+startCountdown();
